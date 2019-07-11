d44fb92684c7c503551e797ca591a39b8ccaac9a
“Dean” <467271883@qq.com>
2019-07-11 18:55:06 +0800

    impletement practice08

--- src/main/java/practice08/Klass.java
+++ src/main/java/practice08/Klass.java
@@ -1,4 +1,34 @@
 package practice08;
 
 public class Klass {
+    private int number;
+    private Student leader;
+
+    public Klass(int number) {
+        this.number = number;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public String getDisplayName(){
+        return String.format("Class %d",this.getNumber());
+    }
+
+    public Student getLeader() {
+        return leader;
+    }
+
+    public void setLeader(Student leader) {
+        this.leader = leader;
+    }
+
+    public void assignLeader(Student student){
+        this.leader = student;
+    }
 }
--- src/main/java/practice08/Teacher.java
+++ src/main/java/practice08/Teacher.java
@@ -1,4 +1,39 @@
 package practice08;
 
-public class Teacher {
+public class Teacher extends Person{
+    private Klass klass;
+
+    public Teacher(String name, int age) {
+        super(name, age);
+    }
+
+    public Teacher(String name, int age, Klass klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public Teacher(int id, String name, int age, Klass klass) {
+        super(id, name, age);
+        this.klass = klass;
+    }
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+    public void setKlass(Klass klass) {
+        this.klass = klass;
+    }
+
+    @Override
+    public String introduce() {
+        String real_klass = klass != null  ? klass.getDisplayName() : "No Class";
+        return super.introduce() + " " + String.format("I am a Teacher. I teach %s.", real_klass);
+    }
+
+    public String introduceWith(Student student){
+        String result;
+        result = klass != null && klass.getNumber() == student.getKlass().getNumber() ? String.format(" I teach %s.", student.getName()) : String.format(" I don't teach %s.", student.getName());
+        return super.introduce() + " I am a Teacher." + result;
+    }
 }
