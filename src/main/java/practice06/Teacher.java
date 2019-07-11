bc6290f913580cf0b2a8e94bd60aaea1dc0dbd21
“Dean” <467271883@qq.com>
2019-07-11 18:16:01 +0800

    impletement practice06

--- src/main/java/practice06/Teacher.java
+++ src/main/java/practice06/Teacher.java
@@ -1,4 +1,28 @@
 package practice06;
 
-public class Teacher {
+public class Teacher extends Person{
+    private int klass;
+
+    public Teacher(String name, int age) {
+        super(name, age);
+    }
+
+    public Teacher(String name, int age, int klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public int getKlass() {
+        return klass;
+    }
+
+    public void setKlass(int klass) {
+        this.klass = klass;
+    }
+
+    @Override
+    public String introduce() {
+        String real_klass = klass == 0 ? "No Class" : String.format("Class %d",klass);
+        return super.introduce() + " " + String.format("I am a Teacher. I teach %s.", real_klass);
+    }
 }
